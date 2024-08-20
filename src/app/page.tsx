002fb0512bd3e8ffe718f60440882711bbb54c12
Remy349 <santoraga15@gmail.com>
2024-08-19 20:13:34 -0600

    feat: Fetch all posts on principal page
    Added ui to handle empty data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
-export default function Page() {
-  return <h1>Home Page</h1>;
+import { getAllPosts } from "@/services/PostService";
+import { EmptyState } from "./_components/empty-state";
+
+export default async function Page() {
+  const posts = await getAllPosts();
+
+  console.log(posts);
+
+  return (
+    <div>
+      {posts.length === 0 ? (
+        <EmptyState />
+      ) : (
+        <div>
+          <h1>CONTENT</h1>
+        </div>
+      )}
+    </div>
+  );
 }
