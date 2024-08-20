0b6dd3e0a48b5302492c93c42388a02fa27a55a8
Remy349 <santoraga15@gmail.com>
2024-08-20 13:24:50 -0600

    feat: New post service added to get a post by id

--- src/services/PostService.ts
+++ src/services/PostService.ts
@@ -1,10 +1,16 @@
 import { GetAllPostsUseCase } from "@/modules/post/application/GetAllPostsUseCase";
+import { GetPostByIdUseCase } from "@/modules/post/application/GetPostByIdUseCase";
 import { PostRepositoryImpl } from "@/modules/post/infrastructure/repository/PrismaPostRepository";
 
 const postRepository = new PostRepositoryImpl();
 
 const getAllPostsUseCase = new GetAllPostsUseCase(postRepository);
+const getPostByIdUseCase = new GetPostByIdUseCase(postRepository);
 
 export const getAllPosts = async () => {
   return await getAllPostsUseCase.execute();
+};
+
+export const getPostById = async (postId: string) => {
+  return await getPostByIdUseCase.execute(postId);
 };
