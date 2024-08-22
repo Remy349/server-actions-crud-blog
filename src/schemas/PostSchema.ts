7d5a569d493b5173e3fd8e24c8d67a4903d4b666
Remy349 <santoraga15@gmail.com>
2024-08-22 13:19:15 -0600

    feat: Add edit form schema and type

--- src/schemas/PostSchema.ts
+++ src/schemas/PostSchema.ts
@@ -5,3 +5,10 @@ export const CreateFormSchema = z.object({
 });
 
 export type TCreateFormSchema = z.infer<typeof CreateFormSchema>;
+
+export const EditFormSchema = z.object({
+  title: z.string().min(1, { message: "Title is required" }),
+  content: z.string().min(1, { message: "Content is required" }),
+});
+
+export type TEditFormSchema = z.infer<typeof EditFormSchema>;
