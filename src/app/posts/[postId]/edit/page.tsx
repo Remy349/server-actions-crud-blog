da3c001161299bf22e2e000fb0fa759ce57c16d6
Remy349 <santoraga15@gmail.com>
2024-08-22 14:55:53 -0600

    feat: Edit post page completed. Better ui added

--- src/app/posts/[postId]/edit/page.tsx
+++ src/app/posts/[postId]/edit/page.tsx
@@ -1,3 +1,73 @@
-export default function Page() {
-  return <h1>Single post edit page</h1>;
+import { Badge } from "@/components/ui/badge";
+import { Button } from "@/components/ui/button";
+import { getPostById } from "@/services/PostService";
+import { ChevronLeft, Eye, EyeOff } from "lucide-react";
+import Link from "next/link";
+import { EditForm } from "./_components/edit-form";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { DeleteDialog } from "./_components/delete-dialog";
+
+interface IParams {
+  params: { postId: string };
+}
+
+export default async function Page({ params }: IParams) {
+  const { postId } = params;
+
+  const post = await getPostById(postId);
+
+  if (!post) {
+    return <h1>Not found</h1>;
+  }
+
+  const plainPost = {
+    id: post.id,
+    title: post.title,
+    content: post.content,
+    isPublished: post.isPublished,
+    createdAt: post.createdAt,
+  };
+
+  return (
+    <div className="md:max-w-3xl md:mx-auto">
+      <div className="flex items-center justify-between mb-6">
+        <div className="flex items-center gap-x-2">
+          <Button size="icon" variant="outline" asChild>
+            <Link href="/">
+              <ChevronLeft className="w-5 h-5" />
+            </Link>
+          </Button>
+          <Button size="icon" variant="outline">
+            {post.isPublished ? (
+              <Eye className="w-4 h-4" />
+            ) : (
+              <EyeOff className="w-4 h-4" />
+            )}
+          </Button>
+          <DeleteDialog postId={post.id} />
+        </div>
+        <Badge variant="secondary">
+          {post.isPublished ? "Published" : "Not published"}
+        </Badge>
+      </div>
+      <Card>
+        <CardHeader>
+          <CardTitle>Edit post</CardTitle>
+          <CardDescription>
+            Here you can edit the information of your post. Click save when you
+            are done.
+          </CardDescription>
+        </CardHeader>
+        <CardContent>
+          <EditForm post={plainPost} />
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
