f2cf94ec01c4a4effd990b2b9a82e65840c3e05a
Remy349 <santoraga15@gmail.com>
2024-08-21 22:28:57 -0600

    feat: Add new delete action for post

--- src/actions/PostAction.ts
+++ src/actions/PostAction.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { CreatePostUseCase } from "@/modules/post/application/CreatePostUseCase";
+import { DeletePostUseCase } from "@/modules/post/application/DeletePostUseCase";
 import { PostRepositoryImpl } from "@/modules/post/infrastructure/repository/PrismaPostRepository";
 import { TCreateFormSchema } from "@/schemas/PostSchema";
 import { revalidatePath } from "next/cache";
@@ -9,6 +10,7 @@ import { redirect } from "next/navigation";
 const postRepository = new PostRepositoryImpl();
 
 const createPostUseCase = new CreatePostUseCase(postRepository);
+const deletePostUseCase = new DeletePostUseCase(postRepository);
 
 export const createPostAction = async (
   formData: TCreateFormSchema,
@@ -19,4 +21,11 @@ export const createPostAction = async (
 
   revalidatePath("/");
   redirect(`/posts/${post.id}/edit`);
+};
+
+export const deletePostAction = async (postId: string): Promise<void> => {
+  await deletePostUseCase.execute(postId);
+
+  revalidatePath("/");
+  redirect("/");
 };
