d2631ee6179f3ce98b50bab5b9dd0e2812b9ace0
Remy349 <santoraga15@gmail.com>
2024-08-22 20:11:34 -0600

    feat: Add update is published post action

--- src/actions/PostAction.ts
+++ src/actions/PostAction.ts
@@ -2,6 +2,7 @@
 
 import { CreatePostUseCase } from "@/modules/post/application/CreatePostUseCase";
 import { DeletePostUseCase } from "@/modules/post/application/DeletePostUseCase";
+import { UpdatePostIsPublishedUseCase } from "@/modules/post/application/UpdatePostIsPublishedUseCase";
 import { PostRepositoryImpl } from "@/modules/post/infrastructure/repository/PrismaPostRepository";
 import { TCreateFormSchema } from "@/schemas/PostSchema";
 import { revalidatePath } from "next/cache";
@@ -11,6 +12,9 @@ const postRepository = new PostRepositoryImpl();
 
 const createPostUseCase = new CreatePostUseCase(postRepository);
 const deletePostUseCase = new DeletePostUseCase(postRepository);
+const updatePostIsPublishedUseCase = new UpdatePostIsPublishedUseCase(
+  postRepository,
+);
 
 export const createPostAction = async (
   formData: TCreateFormSchema,
@@ -19,6 +23,15 @@ export const createPostAction = async (
 
   revalidatePath("/");
   redirect(`/posts/${post.id}/edit`);
+};
+
+export const updatePostIsPublishedAction = async (
+  postId: string,
+  isPublished: boolean,
+): Promise<void> => {
+  await updatePostIsPublishedUseCase.execute(postId, isPublished);
+
+  revalidatePath(`/posts/${postId}/edit`);
 };
 
 export const deletePostAction = async (postId: string): Promise<void> => {
